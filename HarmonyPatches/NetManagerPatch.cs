645a29a3977838a12a7e371673cd93a84aaebea0
Tal <talrofeh@gmail.com>
2025-08-15 20:01:17 +0300

    support fuel lane selection in a radius of gas station

--- HarmonyPatches/NetManagerPatch.cs
+++ HarmonyPatches/NetManagerPatch.cs
@@ -10,7 +10,7 @@ namespace RoadsideCare.HarmonyPatches
     public static class NetManagerPatch
     {
         [HarmonyPatch(typeof(NetManager), "CreateSegment",
-           [typeof(ushort), typeof(Randomizer), typeof(NetInfo), typeof(ushort), typeof(ushort), typeof(Vector3), typeof(Vector3), typeof(uint), typeof(uint), typeof(bool)],
+           [typeof(ushort), typeof(Randomizer), typeof(NetInfo), typeof(TreeInfo), typeof(ushort), typeof(ushort), typeof(Vector3), typeof(Vector3), typeof(uint), typeof(uint), typeof(bool)],
            [ArgumentType.Out, ArgumentType.Ref, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal])]
         [HarmonyPostfix]
         public static void CreateSegment(ref ushort segment, ref Randomizer randomizer, NetInfo info, TreeInfo treeInfo, ushort startNode, ushort endNode, Vector3 startDirection, Vector3 endDirection, uint buildIndex, uint modifiedIndex, bool invert)
@@ -46,31 +46,84 @@ namespace RoadsideCare.HarmonyPatches
                 return;
             }
 
-            Vector3 segPos = segment.m_bounds.center;
-
             for (ushort buildingID = 0; buildingID < BuildingManager.MAX_BUILDING_COUNT; buildingID++)
             {
                 ref Building b = ref BuildingManager.instance.m_buildings.m_buffer[buildingID];
                 if ((b.m_flags & Building.Flags.Created) == 0)
                     continue;
 
-                if (b.Info.m_buildingAI is GasPumpAI && Vector3.Distance(segPos, b.m_position) <= 30f && GasStationManager.GasStationBuildingExist(buildingID))
+                if (b.Info.m_buildingAI is GasPumpAI && GasStationManager.GasStationBuildingExist(buildingID))
                 {
-                    var gasStation = GasStationManager.GetGasStationBuilding(buildingID);
-                    if (isNew)
+                    if(IsSegmentFullyWithinRadius(segmentID, buildingID, 60f))
                     {
-                        if (!gasStation.FuelLanes.Contains(segmentID))
+                        var gasStation = GasStationManager.GetGasStationBuilding(buildingID);
+                        if (isNew)
                         {
-                            gasStation.FuelLanes.Add(segmentID);
-                        }    
+                            if (!gasStation.FuelLanes.Contains(segmentID))
+                            {
+                                gasStation.FuelLanes.Add(segmentID);
+                            }
+                        }
+                        else
+                        {
+                            gasStation.FuelLanes.Remove(segmentID);
+                        }
+                        GasStationManager.SetFuelLanes(buildingID, gasStation.FuelLanes);
                     }
-                    else
-                    {
-                        gasStation.FuelLanes.Remove(segmentID);
-                    }
-                    GasStationManager.SetFuelLanes(buildingID, gasStation.FuelLanes);
                 }
             }
+        }
+
+        private static bool IsSegmentFullyWithinRadius(ushort segmentId, ushort buildingId, float radius)
+        {
+            Vector3 buildingPos = BuildingManager.instance.m_buildings.m_buffer[buildingId].m_position;
+
+            // Get Bezier curve of the segment
+            Bezier3 bezier = GetSegmentBezier(segmentId);
+
+            // Number of samples along the curve
+            int samples = 8; // increase for more accuracy
+            float radiusSq = radius * radius;
+
+            for (int i = 0; i <= samples; i++)
+            {
+                float t = i / (float)samples;
+                Vector3 pos = bezier.Position(t);
+
+                // If any point is outside radius, the segment is not fully inside
+                if ((pos - buildingPos).sqrMagnitude > radiusSq)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static Bezier3 GetSegmentBezier(ushort segmentId)
+        {
+            ref NetSegment segment = ref NetManager.instance.m_segments.m_buffer[segmentId];
+
+            Vector3 startPos = NetManager.instance.m_nodes.m_buffer[segment.m_startNode].m_position;
+            Vector3 endPos = NetManager.instance.m_nodes.m_buffer[segment.m_endNode].m_position;
+
+            Vector3 startDir = segment.m_startDirection;
+            Vector3 endDir = segment.m_endDirection;
+
+            NetSegment.CalculateMiddlePoints(
+                startPos, startDir,
+                endPos, endDir,
+                smoothStart: false, // use true if you want smoothing
+                smoothEnd: false,
+                out Vector3 middlePos1,
+                out Vector3 middlePos2
+            );
+
+            Bezier3 bezier;
+            bezier.a = startPos;
+            bezier.b = middlePos1;
+            bezier.c = middlePos2;
+            bezier.d = endPos;
+
+            return bezier;
         }
     }
 }
