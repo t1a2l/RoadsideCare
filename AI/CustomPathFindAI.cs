df663a3db5a91de1030c4695ab10bb8509328a1c
Tal <talrofeh@gmail.com>
2025-08-24 11:57:17 +0300

    better custom path find

--- AI/CustomPathFindAI.cs
+++ AI/CustomPathFindAI.cs
@@ -23,128 +23,133 @@ namespace RoadsideCare.AI
             {
                 if (vehicleData.m_sourceBuilding != 0)
                 {
-                    if (TryGetRandomLaneTarget(vehicleID, vehicleData.m_sourceBuilding, out Vector3 targetPos))
+                    BuildingManager instance = Singleton<BuildingManager>.instance;
+                    BuildingInfo info = instance.m_buildings.m_buffer[vehicleData.m_sourceBuilding].Info;
+
+                    if (info.GetAI() is GasPumpAI && GasStationManager.GasStationBuildingExist(vehicleData.m_targetBuilding))
                     {
-                        // Use fuel lane position as path target
-                        return StartPathFindCargoTruckAI(Singleton<CargoTruckAI>.instance, vehicleID, ref vehicleData, vehicleData.m_targetPos3, targetPos, true, true, false);
+                        if (TryFindRandomGasPumpPoint(vehicleID, vehicleData.m_targetBuilding, out Vector3 fuelPointTargetPos))
+                        {
+                            return StartPathFindCargoTruckAI(Singleton<CargoTruckAI>.instance, vehicleID, ref vehicleData, vehicleData.m_targetPos3, fuelPointTargetPos, true, true, false);
+                        }
                     }
-                    else
+                    if (info.GetAI() is VehicleWashBuildingAI && VehicleWashBuildingManager.VehicleWashBuildingExist(vehicleData.m_targetBuilding))
                     {
-                        BuildingManager instance = Singleton<BuildingManager>.instance;
-                        BuildingInfo info = instance.m_buildings.m_buffer[vehicleData.m_sourceBuilding].Info;
-                        Randomizer randomizer = new(vehicleID);
-                        info.m_buildingAI.CalculateUnspawnPosition(vehicleData.m_sourceBuilding, ref instance.m_buildings.m_buffer[vehicleData.m_sourceBuilding], ref randomizer, m_info, out Vector3 a, out Vector3 target);
-                        return StartPathFindCargoTruckAI(Singleton<CargoTruckAI>.instance, vehicleID, ref vehicleData, vehicleData.m_targetPos3, target, true, true, false);
-                    }   
+                        if (TryFindRandomVehicleWashPoint(vehicleID, vehicleData.m_targetBuilding, out Vector3 vehicleWashPointTargetPos))
+                        {
+                            var result = StartPathFindCargoTruckAI(Singleton<CargoTruckAI>.instance, vehicleID, ref vehicleData, vehicleData.m_targetPos3, vehicleWashPointTargetPos, true, true, false);
+                            if (!result)
+                            {
+                                if (TryFindRandomVehicleTunnelWash(vehicleID, vehicleData.m_targetBuilding, out Vector3 vehicleWashLaneTargetPos))
+                                {
+                                    return StartPathFindCargoTruckAI(Singleton<CargoTruckAI>.instance, vehicleID, ref vehicleData, vehicleData.m_targetPos3, vehicleWashLaneTargetPos, true, true, false);
+                                }
+                            }
+                        }
+                    }
+                    Randomizer randomizer = new(vehicleID);
+                    info.m_buildingAI.CalculateUnspawnPosition(vehicleData.m_sourceBuilding, ref instance.m_buildings.m_buffer[vehicleData.m_sourceBuilding], ref randomizer, m_info, out Vector3 a, out Vector3 target);
+                    return StartPathFindCargoTruckAI(Singleton<CargoTruckAI>.instance, vehicleID, ref vehicleData, vehicleData.m_targetPos3, target, true, true, false);
                 }
             }
             else if (vehicleData.m_targetBuilding != 0)
             {
-                if (TryGetRandomLaneTarget(vehicleID, vehicleData.m_targetBuilding, out Vector3 targetPos))
+                BuildingManager instance2 = Singleton<BuildingManager>.instance;
+                BuildingInfo info2 = instance2.m_buildings.m_buffer[vehicleData.m_targetBuilding].Info;
+
+                if(info2.GetAI() is GasPumpAI && GasStationManager.GasStationBuildingExist(vehicleData.m_targetBuilding))
                 {
-                    // Use random lane position as path target
-                    return StartPathFindCargoTruckAI(Singleton<CargoTruckAI>.instance, vehicleID, ref vehicleData, vehicleData.m_targetPos3, targetPos, true, true, false);
+                    if(TryFindRandomGasPumpPoint(vehicleID, vehicleData.m_targetBuilding, out Vector3 fuelPointTargetPos))
+                    {
+                        return StartPathFindCargoTruckAI(Singleton<CargoTruckAI>.instance, vehicleID, ref vehicleData, vehicleData.m_targetPos3, fuelPointTargetPos, true, true, false);
+                    }
                 }
-                else
+                if (info2.GetAI() is VehicleWashBuildingAI && VehicleWashBuildingManager.VehicleWashBuildingExist(vehicleData.m_targetBuilding))
                 {
-                    BuildingManager instance2 = Singleton<BuildingManager>.instance;
-                    BuildingInfo info2 = instance2.m_buildings.m_buffer[vehicleData.m_targetBuilding].Info;
-                    Randomizer randomizer2 = new(vehicleID);
-                    info2.m_buildingAI.CalculateUnspawnPosition(vehicleData.m_targetBuilding, ref instance2.m_buildings.m_buffer[vehicleData.m_targetBuilding], ref randomizer2, m_info, out Vector3 b, out Vector3 target2);
-                    return StartPathFindCargoTruckAI(Singleton<CargoTruckAI>.instance, vehicleID, ref vehicleData, vehicleData.m_targetPos3, target2, true, true, false);
-                } 
+                    if (TryFindRandomVehicleWashPoint(vehicleID, vehicleData.m_targetBuilding, out Vector3 vehicleWashPointTargetPos))
+                    {
+                        var result = StartPathFindCargoTruckAI(Singleton<CargoTruckAI>.instance, vehicleID, ref vehicleData, vehicleData.m_targetPos3, vehicleWashPointTargetPos, true, true, false);
+                        if(!result)
+                        {
+                            if (TryFindRandomVehicleTunnelWash(vehicleID, vehicleData.m_targetBuilding, out Vector3 vehicleWashLaneTargetPos))
+                            {
+                                return StartPathFindCargoTruckAI(Singleton<CargoTruckAI>.instance, vehicleID, ref vehicleData, vehicleData.m_targetPos3, vehicleWashLaneTargetPos, true, true, false);
+                            }
+                        }
+                    }
+                }
+                Randomizer randomizer2 = new(vehicleID);
+                info2.m_buildingAI.CalculateUnspawnPosition(vehicleData.m_targetBuilding, ref instance2.m_buildings.m_buffer[vehicleData.m_targetBuilding], ref randomizer2, m_info, out Vector3 b, out Vector3 target2);
+                return StartPathFindCargoTruckAI(Singleton<CargoTruckAI>.instance, vehicleID, ref vehicleData, vehicleData.m_targetPos3, target2, true, true, false);
             }
             return false;
         }
 
-        public static bool TryGetRandomLaneTarget(ushort vehicleID, ushort buildingID, out Vector3 targetPos)
+        private static bool TryFindRandomGasPumpPoint(ushort vehicleID, ushort buildingID, out Vector3 targetPos)
         {
             targetPos = Vector3.zero;
-            var building = BuildingManager.instance.m_buildings.m_buffer[buildingID];
-            if ((building.m_flags & Building.Flags.Created) == 0) return false;
+            var gasStation = GasStationManager.GetGasStationBuilding(buildingID);
 
-            if(building.Info.m_buildingAI is GasPumpAI && GasStationManager.GasStationBuildingExist(buildingID))
+            if (gasStation.FuelPoints == null || gasStation.FuelPoints.Count == 0)
             {
-                var gasStation = GasStationManager.GetGasStationBuilding(buildingID);
-
-                if (gasStation.FuelPoints == null || gasStation.FuelPoints.Count == 0)
-                {
-                    return false;
-                }
-
-                // Pick a random segment from the fuel lanes
-                ushort segmentId = gasStation.FuelPoints[Random.Range(0, gasStation.FuelPoints.Count)];
-                var laneId = NetManager.instance.m_segments.m_buffer[segmentId].m_lanes; // single lane index 0
-
-                if (laneId == 0) return false;
-
-                // Get lane position for pathfinding target
-                targetPos = NetManager.instance.m_lanes.m_buffer[laneId].CalculatePosition(0.5f); // middle of lane
-                VehicleNeedsManager.SetIsGoingToRefuelMode(vehicleID);
-                return true;
+                return false;
             }
-            else if (building.Info.m_buildingAI is VehicleWashBuildingAI && VehicleWashBuildingManager.VehicleWashBuildingExist(buildingID))
-            {
-                var vehicleWashBuilding = VehicleWashBuildingManager.GetVehicleWashBuilding(buildingID);
 
-                var washLanesExist = vehicleWashBuilding.VehicleWashLanes != null && vehicleWashBuilding.VehicleWashLanes.Count > 0;
+            // Pick a random segment from the fuel points
+            ushort segmentId = gasStation.FuelPoints[Random.Range(0, gasStation.FuelPoints.Count)];
+            var laneId = NetManager.instance.m_segments.m_buffer[segmentId].m_lanes; // single lane index 0
 
-                var washPointsExist = vehicleWashBuilding.VehicleWashPoints != null && vehicleWashBuilding.VehicleWashPoints.Count > 0;
+            if (laneId == 0) return false;
 
-                var selectWashLane = false;
-
-                var selectWashPoint = false;
-
-                if (washLanesExist && washPointsExist)
-                {
-                    bool chooseRandom = Singleton<SimulationManager>.instance.m_randomizer.Int32(1U) == 0;
-                    if (chooseRandom)
-                    {
-                        selectWashLane = true;
-                    }
-                    else
-                    {
-                        selectWashPoint = true;
-                    }
-                }
-                else if (washLanesExist)
-                {
-                    selectWashLane = true;
-                }
-                else if (washPointsExist)
-                {
-                    selectWashPoint = true;
-                }
-
-                if (selectWashLane)
-                {
-                    // Pick a random segment from the fuel lanes
-                    ushort segmentId = vehicleWashBuilding.VehicleWashLanes[Random.Range(0, vehicleWashBuilding.VehicleWashLanes.Count)];
-                    var laneId = NetManager.instance.m_segments.m_buffer[segmentId].m_lanes; // single lane index 0
-
-                    if (laneId == 0) return false;
-
-                    // Get lane position for pathfinding target
-                    targetPos = NetManager.instance.m_lanes.m_buffer[laneId].CalculatePosition(1f); // end of lane
-                    VehicleNeedsManager.SetIsGoingToTunnelWashMode(vehicleID);
-                    return true;
-                }
-
-                if (selectWashPoint)
-                {
-                    // Pick a random segment from the fuel lanes
-                    ushort segmentId = vehicleWashBuilding.VehicleWashPoints[Random.Range(0, vehicleWashBuilding.VehicleWashPoints.Count)];
-                    var laneId = NetManager.instance.m_segments.m_buffer[segmentId].m_lanes; // single lane index 0
-
-                    if (laneId == 0) return false;
-
-                    // Get lane position for pathfinding target
-                    targetPos = NetManager.instance.m_lanes.m_buffer[laneId].CalculatePosition(0.5f); // middle of lane
-                    VehicleNeedsManager.SetIsGoingToHandWashMode(vehicleID);
-                    return true;
-                }
-            }
-            return false;
+            // Get lane position for pathfinding target
+            targetPos = NetManager.instance.m_lanes.m_buffer[laneId].CalculatePosition(0.5f); // middle of lane
+            VehicleNeedsManager.SetIsGoingToRefuelMode(vehicleID);
+            return true;
         }
+
+        private static bool TryFindRandomVehicleWashPoint(ushort vehicleID, ushort buildingID, out Vector3 targetPos)
+        {
+            targetPos = Vector3.zero;
+            var vehicleWashBuilding = VehicleWashBuildingManager.GetVehicleWashBuilding(buildingID);
+
+            if (vehicleWashBuilding.VehicleWashPoints == null || vehicleWashBuilding.VehicleWashPoints.Count == 0)
+            {
+                return false;
+            }
+
+            // Pick a random segment from the vehicle wash points
+            ushort segmentId = vehicleWashBuilding.VehicleWashPoints[Random.Range(0, vehicleWashBuilding.VehicleWashPoints.Count)];
+            var laneId = NetManager.instance.m_segments.m_buffer[segmentId].m_lanes; // single lane index 0
+
+            if (laneId == 0) return false;
+
+            // Get lane position for pathfinding target
+            targetPos = NetManager.instance.m_lanes.m_buffer[laneId].CalculatePosition(0.5f); // middle of lane
+            VehicleNeedsManager.SetIsGoingToHandWashMode(vehicleID);
+            return true;
+        }
+
+        private static bool TryFindRandomVehicleTunnelWash(ushort vehicleID, ushort buildingID, out Vector3 targetPos)
+        {
+            targetPos = Vector3.zero;
+            var vehicleWashBuilding = VehicleWashBuildingManager.GetVehicleWashBuilding(buildingID);
+
+            if (vehicleWashBuilding.VehicleWashLanes == null || vehicleWashBuilding.VehicleWashLanes.Count == 0)
+            {
+                return false;
+            }
+
+            // Pick a random segment from the vehicle wash lanes
+            ushort segmentId = vehicleWashBuilding.VehicleWashLanes[Random.Range(0, vehicleWashBuilding.VehicleWashLanes.Count)];
+            var laneId = NetManager.instance.m_segments.m_buffer[segmentId].m_lanes; // single lane index 0
+
+            if (laneId == 0) return false;
+
+            // Get lane position for pathfinding target
+            targetPos = NetManager.instance.m_lanes.m_buffer[laneId].CalculatePosition(1f); // end of lane
+            VehicleNeedsManager.SetIsGoingToHandWashMode(vehicleID);
+            return true;
+        }
+
     }
 }
