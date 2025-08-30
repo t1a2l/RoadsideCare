dddeccbc485f6205853dacc506bb6a94ef84aa3e
Tal <talrofeh@gmail.com>
2025-08-30 23:17:04 +0300

    remove

--- HarmonyPatches/CarAIPatch.cs
+++ HarmonyPatches/CarAIPatch.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 using ColossalFramework;
 using HarmonyLib;
 using MoreTransferReasons;
@@ -17,36 +16,6 @@ namespace RoadsideCare.HarmonyPatches
         private const float SERVICE_DISTANCE_THRESHOLD = 80f;
         private const float PROXIMITY_THRESHOLD = 10f;
         private const float STEPS_PER_SECOND = 4f;
-
-        [HarmonyPatch(typeof(CarAI), "PathfindFailure")]
-        [HarmonyPostfix]
-        public static void PathfindFailure(ushort vehicleID, ref Vehicle data)
-        {
-            if (VehicleNeedsManager.VehicleNeedsExist(vehicleID))
-            {
-                var vehicleNeeds = VehicleNeedsManager.GetVehicleNeeds(vehicleID);
-                var targetBuilding = vehicleNeeds.OriginalTargetBuilding;
-
-                if (data.Info.m_vehicleAI is CargoTruckAI cargoTruckAI && (data.m_targetBuilding != 0))
-                {
-                    cargoTruckAI.SetTarget(vehicleID, ref data, targetBuilding);
-                }
-                else if (data.Info.m_vehicleAI is PassengerCarAI passengerCarAI && (data.m_targetBuilding != 0))
-                {
-                    passengerCarAI.SetTarget(vehicleID, ref data, targetBuilding);
-                }
-            }
-        }
-
-        [HarmonyPatch(typeof(CarAI), "StartPathFind",
-           [typeof(ushort), typeof(Vehicle), typeof(Vector3), typeof(Vector3), typeof(bool), typeof(bool), typeof(bool)],
-           [ArgumentType.Normal, ArgumentType.Ref, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal])]
-        [HarmonyReversePatch]
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        public static bool BaseCarAIStartPathFind(CarAI instance, ushort vehicleID, ref Vehicle vehicleData, Vector3 startPos, Vector3 endPos, bool startBothWays, bool endBothWays, bool undergroundTarget)
-        {
-            return false;
-        }
 
         public static void ArriveAtTarget(CarAI instance, ushort vehicleID, ref Vehicle data)
         {
