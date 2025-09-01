744625404e71ca153656114e202d271c516b5621
Tal <talrofeh@gmail.com>
2025-09-01 22:41:56 +0300

    add lower speed to fuel points and wash points so cars will not path through them

--- AI/FuelPointAI.cs
+++ AI/FuelPointAI.cs
@@ -2,5 +2,24 @@
 {
     public class FuelPointAI : RoadAI
     {
+        public override void CreateSegment(ushort segmentID, ref NetSegment data)
+        {
+            base.CreateSegment(segmentID, ref data);
+            NetInfo.Lane[] lanes = data.Info.m_lanes;
+            lanes[0].m_speedLimit = 0.1f;
+        }
+
+        public override void SegmentLoaded(ushort segmentID, ref NetSegment data, uint version)
+        {
+            base.SegmentLoaded(segmentID, ref data, version);
+            NetInfo.Lane[] lanes = data.Info.m_lanes;
+            lanes[0].m_speedLimit = 0.1f;
+        }
+
+        public static float GetMaxSpeed(ushort segmentID, ref NetSegment data)
+        {
+            NetInfo.Lane[] lanes = data.Info.m_lanes;
+            return lanes[0].m_speedLimit;
+        }
     }
 }
