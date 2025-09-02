e9aae084236f85f77cad1b5cb7c42ef48121194c
Tal <talrofeh@gmail.com>
2025-09-03 02:20:56 +0300

    avoid driving through fuel and wash roads to target

---
+++ HarmonyPatches/PathFindPatch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using RoadsideCare.AI;
+
+namespace RoadsideCare.HarmonyPatches
+{
+    [HarmonyPatch]
+    public static class PathFindPatch
+    {
+        [HarmonyPatch(typeof(PathFind), "CalculateLaneSpeed")]
+        [HarmonyPostfix]
+        public static void CalculateLaneSpeed(byte startOffset, byte endOffset, ref NetSegment segment, NetInfo.Lane laneInfo, ref float __result)
+        {
+            bool isFuelPoint = segment.Info.m_netAI is FuelPointAI || segment.Info.m_netAI is FuelPointSmallAI || segment.Info.m_netAI is FuelPointLargeAI;
+            bool isVehicleWashPoint = segment.Info.m_netAI is VehicleWashPointAI || segment.Info.m_netAI is VehicleWashPointSmallAI || segment.Info.m_netAI is VehicleWashPointLargeAI;
+            bool isVehicleWashLane = segment.Info.m_netAI is VehicleWashLaneAI || segment.Info.m_netAI is VehicleWashLaneSmallAI || segment.Info.m_netAI is VehicleWashLaneLargeAI;
+            if (isFuelPoint || isVehicleWashPoint || isVehicleWashLane)
+            {
+                __result /= 100;
+            }
+        }
+    }
+}
